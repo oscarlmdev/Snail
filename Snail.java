bae88c1a1f66da50e7a8d54d549c9455f69e4d65
Óscar Lacoma Marrodán <146749612+oscarlmdev@users.noreply.github.com>
2024-02-19 13:46:52 +0100

    Add files via upload

---
+++ Snail.java
@@ -0,0 +1,48 @@
+public class Snail {
+
+    public static int[] snail(int[][] array) {
+
+        // Case empty array
+        if (array.length == 1) return array[0];
+
+        int[] result = new int[array.length * array.length];
+        int i = 0;
+
+        int rTop = 0;
+        int rBottom = array.length - 1;
+        int cLeft = 0;
+        int cRight = array.length - 1;
+
+        while ((rTop <= rBottom) && (cLeft <= cRight)) {
+
+            // Row left to right
+            for (int r = cLeft; r <= cRight; r++) {
+                result[i++] = array[rTop][r];
+            }
+            rTop++;
+
+            // Column top to bottom
+            for (int c = rTop; c <= rBottom; c++) {
+                result[i++] = array[c][cRight];
+            }
+            cRight--;
+
+            // Row right to left
+            if (rTop <= rBottom) {
+                for (int r = cRight; r >= cLeft; r--) {
+                    result[i++] = array[rBottom][r];
+                }
+                rBottom--;
+
+                // Column bottom to top
+            }
+            if (cLeft <= cRight) {
+                for (int c = rBottom; c >= rTop; c--) {
+                    result[i++] = array[c][cLeft];
+                }
+                cLeft++;
+            }
+        }
+        return result;
+    }
+}
